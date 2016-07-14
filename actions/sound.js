c3e21e7dd946aaf4ccf49f2a8bc27d3fc226f3f1
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-14 14:39:53 -0300

    Unlock sound

--- actions/sound.js
+++ actions/sound.js
@@ -1,6 +1,6 @@
 module.exports = function(){
 	var Sound = require('node-aplay');
-	var music = new Sound('../media/dingdong.wav');
+	var music = new Sound('./../media/dingdong.wav');
 
 	music.play();
 
