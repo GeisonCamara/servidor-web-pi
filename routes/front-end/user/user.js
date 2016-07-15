3d75f5a7f04222f7aaea49cd4b6c05f8f331a06e
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-15 10:40:06 -0300

    tela de usuários

--- routes/front-end/user/user.js
+++ routes/front-end/user/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Raspberry PI' });
+    res.render('user', { title: 'Raspberry PI' });
 });
 
 module.exports = router;
