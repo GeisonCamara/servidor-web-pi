e1c1f7c07b052d30023c0d84323155de5bc0034d
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-15 16:38:03 -0300

    Interação do botão

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -10,6 +10,7 @@ function clickButton(){
 	$('#button').click(function(){
     	var state = $('#button').is(":checked");
     	$(this).prop('disabled', true);
+    	console.log('foi');
     	if(state===true){
     		setTimeout(function(){
     			$('#button').attr('checked', false);
