e0ce2de52cf1418d7900ad84dd206d6521d4939c
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-15 14:05:11 -0300

    Tela de cadastro

--- routes/front-end/user/insert.js
+++ routes/front-end/user/insert.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 var mongo = require('./../../../classes/mongo/mongo.js');
 
+router.get('/', function(req, res, next) {
+    res.render('insert', { title: 'Server Raspberry PI' });
+});
+
 router.post('/', function(req, res, next) {
     if(req.query.hasOwnProperty('user')){
         var insertObj = {name: req.query.user, key: "", mobile: req.query.mobile, nfc: "", status: "A"};
