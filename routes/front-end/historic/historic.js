cc3360b27dc3ffb73a52f9100a5bfde002543673
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-18 16:01:15 -0300

    Tela de histórico

---
+++ routes/front-end/historic/historic.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+//var mongo = require('./../../../classes/mongo/mongo.js');
+
+router.get('/', function(req, res, next) {
+    res.render('historic', { title: 'Server Raspberry PI' });
+});
