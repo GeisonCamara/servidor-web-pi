536baad4e6add04d06391109c73dd7c172e8b084
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-18 13:08:33 -0300

    Função para destravar

--- routes/authentication/mobile.js
+++ routes/authentication/mobile.js
@@ -6,7 +6,7 @@ Senha.get('/', function(req, res){
     var key = req.query.key;
     Mongo.find({mobile:key}, 'user', res, function(res, userObj){
       	if(userObj.length > 0){
-      		res.redirect("./../unlock?key=DD2016TRNEE&device=mobile&user="+userObj.name);
+      		res.redirect("./../unlock?key=DD2016TRNEE&device=mobile&user="+userObj[0]);
       	}
     });
 });
