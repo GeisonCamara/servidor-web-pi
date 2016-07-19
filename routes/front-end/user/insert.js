e3c91a5301a20ca7b0dcabf204b982c6f50147a1
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-19 10:52:48 -0300

    random password

--- routes/front-end/user/insert.js
+++ routes/front-end/user/insert.js
@@ -8,7 +8,9 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     if(req.query.hasOwnProperty('user')){
-        var insertObj = {name: req.query.user, key: req.query.key, mobile: req.query.mobile, nfc: req.query.nfc, status: "A"};
+    	var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    	var password = Array.apply(null, Array(N)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
+        var insertObj = {name: req.query.user, key: req.query.key, mobile: password, nfc: req.query.nfc, status: "A"};
         mongo.insert(insertObj, 'user', function(){});
         res.sendStatus(200);
     }
