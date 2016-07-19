db26814227f8a26d7e225f27ca6a71a5ac17bf74
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-19 14:07:15 -0300

    tabela usuários

--- routes/front-end/user/insert.js
+++ routes/front-end/user/insert.js
@@ -10,7 +10,7 @@ router.post('/', function(req, res, next) {
     if(req.query.hasOwnProperty('user')){
     	var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     	var password = Array.apply(null, Array(5)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
-        var insertObj = {name: req.query.user, key: req.query.key, mobile: password, nfc: req.query.nfc, status: "A"};
+        var insertObj = {name: req.query.user, role: "adm", status: "A", devices: [{status: "A", ID: "key", value: "1234", timeRange: ""}, {status: "A", ID: "mobile", value: password, timeRange: ""}, {status: "I", ID: "nfc", value: "abcd", timeRange: ""}]};
         mongo.insert(insertObj, 'user', function(){});
         res.sendStatus(200);
     }
