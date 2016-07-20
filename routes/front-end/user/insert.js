adcb66279c8c137420294f989d37e56035b9db93
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-20 14:45:11 -0300

    user insert

--- routes/front-end/user/insert.js
+++ routes/front-end/user/insert.js
@@ -8,6 +8,8 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
     if(req.query.hasOwnProperty('token')){
+        mongo.find({});
+
     	var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     	var password = Array.apply(null, Array(5)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
         var insertObj = {name: req.query.user, role: "adm", status: "A", devices: [{status: "A", ID: "key", value: "1234", timeRange: ""}, {status: "A", ID: "mobile", value: password, timeRange: ""}, {status: "I", ID: "nfc", value: "abcd", timeRange: ""}]};
