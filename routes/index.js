72c5c62cdce3383f49def47c085d6bdf0ad7e03e
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-21 15:39:01 -0300

    configurações

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,46 @@ var Gpio = require('onoff').Gpio,
 vcc.writeSync(1);
 rele.writeSync(1);
 
+
 var nconf = require('nconf');
+var configuracoes = {campainhaStatus:1,
+                    toque:"dingdong.wav",
+                    nfc:0,
+                    camera:0,
+                    btnInterno:1,
+                    mobile:1,
+                    microfone:0,
+                    touch:0};
+
 nconf.argv()
     .env()
     .file({ file: 'data.json' });
+
+nconf.set('campainha:status', configuracoes.campainhaStatus);
+nconf.set('campainha:toque', configuracoes.toque);
+nconf.set('nfc', configuracoes.nfc);
+nconf.set('camera', configuracoes.camera);
+nconf.set('btnInterno', configuracoes.btnInterno);
+nconf.set('mobile', configuracoes.mobile);
+nconf.set('microfone', configuracoes.microfone);
+nconf.set('touch', configuracoes.touch);
+            
+console.log('status: ' + nconf.get('campainhaStatus'));
+console.log('toque: ' + nconf.get('toque'));
+console.log('nfc: ' + nconf.get('nfc'));
+console.log('camera: ' + nconf.get('camera'));
+console.log('btnInterno: ' + nconf.get('btnInterno'));
+console.log('mobile: ' + nconf.get('mobile'));
+console.log('microfone: ' + nconf.get('microfone'));
+console.log('touch: ' + nconf.get('touch'));
+
+
+nconf.save(function (err) {
+    //fs.readFile('data.json', function (err, data) {
+          //console.dir(JSON.parse(data.toString()))
+    //});
+});
+
 
 
 console.log(nconf.get('btnInterno'));
