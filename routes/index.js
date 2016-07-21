5d926c2a2a35857c015e806025a5d57cf4691f48
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-21 11:09:38 -0300

    configurações

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var lock = require('./../actions/lock.js');
 var sound = require('./../actions/sound.js');
+var config = require('./../classes/config.js');
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Server Raspberry PI' });
@@ -17,28 +18,34 @@ var flag = 1;
 vcc.writeSync(1);
 rele.writeSync(1);
 
-button.watch(function(err, value){
-    if(flag){
+config();
+
+if(config.get('btnInterno')==1){
+    button.watch(function(err, value){
+        if(flag){
+            if (err) {
+                throw err;
+            }
+            if(value==1){
+                lock('buttonOut', 'Exit button');
+            }
+            flag = 0;
+            setTimeout(function(){
+                flag = 1;
+            },3100);
+        }
+    });
+}
+
+if(config.get('campainha:status')==1){
+    dingdong.watch(function(err, value){
         if (err) {
             throw err;
         }
         if(value==1){
-            lock('buttonOut', 'Exit button');
+            sound();
         }
-        flag = 0;
-        setTimeout(function(){
-            flag = 1;
-        },3100);
-    }
-});
-
-dingdong.watch(function(err, value){
-    if (err) {
-        throw err;
-    }
-    if(value==1){
-        sound();
-    }
-});
+    });
+}
 
 module.exports = router;
