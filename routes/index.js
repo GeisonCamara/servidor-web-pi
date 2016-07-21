af2b7228b01fa4decb9121c8d4ac88b0a9943e2a
marcelobohn <marcelo.bohn@gmail.com>
2016-07-21 10:35:52 -0300

    TimeOut

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var lock = require('./../actions/lock.js');
 var sound = require('./../actions/sound.js');
+var sleep = require('sleep');
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Server Raspberry PI' });
@@ -11,25 +12,18 @@ var Gpio = require('onoff').Gpio,
     button = new Gpio(21, 'in', 'falling'),
     dingdong = new Gpio(19, 'in', 'falling'),
     vcc = new Gpio(26, 'out'),
-    rele = new Gpio(16, 'out');
-var flag = 1;    
+    rele = new Gpio(16, 'out');    
 
 vcc.writeSync(1);
 rele.writeSync(1);
 
 button.watch(function(err, value){
-    if(flag){
-        if (err) {
-            throw err;
-        }
-        if(value==1){
-            lock('buttonOut', 'Exit button');
-        }
-        flag = 0;
-        setTimeout(function(){
-            flag = 1;
-        },3100);
+    if (err) {
+        throw err;
     }
+    if(value==1){
+        lock('buttonOut', 'Exit button');
+    } 
 });
 
 dingdong.watch(function(err, value){
