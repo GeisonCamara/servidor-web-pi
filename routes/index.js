3b34a6ca5696756fe83e6992e5e4c39f721cede1
Bruno Agostini Marques <bruno.marques@digitaldesk.com.br>
2016-07-21 14:23:55 -0300

    configurações

--- routes/index.js
+++ routes/index.js
@@ -12,34 +12,11 @@ var Gpio = require('onoff').Gpio,
     button = new Gpio(21, 'in', 'falling'),
     dingdong = new Gpio(19, 'in', 'falling'),
     vcc = new Gpio(26, 'out'),
-<<<<<<< f7b4a1563a7e3ce722b832bdc66b3cd71141ccce
-    rele = new Gpio(16, 'out');    
-=======
     rele = new Gpio(16, 'out');
->>>>>>> configurações
 
 vcc.writeSync(1);
 rele.writeSync(1);
 
-<<<<<<< d9f3fb399165257a53c25c3151928bd03444201d
-button.watch(function(err, value){
-    if (err) {
-        throw err;
-    }
-    if(value==1){
-        lock('buttonOut', 'Exit button');
-    } 
-});
-
-dingdong.watch(function(err, value){
-    if (err) {
-        throw err;
-    }
-    if(value==1){
-        sound();
-    }
-});
-=======
 config();
 
 if(config.get('btnInterno')==1){
@@ -63,6 +40,5 @@ if(config.get('campainha:status')==1){
         }
     });
 }
->>>>>>> configurações
 
 module.exports = router;
