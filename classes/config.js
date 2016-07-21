c81e40fa82f00caefba21685fc28a06754a2cbcb
Geison Camara <geison.camara@digitaldesk.com.br>
2016-07-21 15:16:36 -0300

    Configurações

--- classes/config.js
+++ classes/config.js
@@ -1,17 +1,15 @@
 //var fs = require('fs');
-var nconf = require('nconf');
-
-nconf.argv()
-    .env()
-    .file({ file: './../config/config.json' });
 
 function config(){
-    
+    this.nconf = require('nconf');
+    this.nconf.argv()
+        .env()
+        .file({ file: './../config/config.json' });    
 }
 
 config.prototype.set = function(config, status){
-    nconf.set(config, status);
-    nconf.save(function (err) {
+    this.nconf.set(config, status);
+    this.nconf.save(function (err) {
         //fs.readFile('data.json', function (err, data) {
               //console.dir(JSON.parse(data.toString()))
         //});
@@ -20,8 +18,8 @@ config.prototype.set = function(config, status){
 }
 
 config.prototype.get = function(config){
-    console.log(config);
-    return nconf.get(config);
+    console.log(this.nconf.get(config));
+    return this.nconf.get(config);
 }
 
 module.exports = new config();
