d4e482c6620d8274f94539408dbc8d41f2d4e787
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-10 16:54:39 -0300

    testes

--- routes/authentication/abrirPorta.js
+++ routes/authentication/abrirPorta.js
@@ -8,8 +8,11 @@ token.post('/', function(req, res){
     Mongo.find({devices: { $elemMatch: { value: tokenGoogle } }}, 'user', res, function(res, userObj){
     	lock('mobile', userObj[0].name);
     	res.send({status: true});
+    	console.log('porta abriu');
     },function(req, res){
-        res.send({status: false});
+        //res.send({status: false});
+        console.log('');
+        console.log('token fail');
     });
 });
 
