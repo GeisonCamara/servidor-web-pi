7e5cf8c37cea197ea82c7603d3a4a550b4fb0bba
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-10 16:30:57 -0300

    enviando nome correto para o histórico

--- routes/authentication/abrirPorta.js
+++ routes/authentication/abrirPorta.js
@@ -6,7 +6,6 @@ var lock = require('./../../actions/lock.js');
 token.post('/', function(req, res){
     var tokenGoogle = req.query.token;
     Mongo.find({devices: { $elemMatch: { value: tokenGoogle } }}, 'user', res, function(res, userObj){
-    	console.log('value - ' + JSON.stringify(userObj[0].name));
     	lock('mobile', userObj[0].name);
     	res.send({status: true});
     },function(req, res){
