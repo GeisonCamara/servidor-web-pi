ddef54f668f0775c25e71f6ac61b31e071176e49
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-11 10:37:49 -0300

    tentando desbugar

--- routes/authentication/mobile.js
+++ routes/authentication/mobile.js
@@ -92,10 +92,10 @@ function conferirToken(req, res, token, access_token, name){
 function atualizarToken(req, res, access_token, name){
     Mongo.update({name: name}, 'user', req, function(userObj, req){
         userObj.devices[1].value = access_token;
-        return userObj;
         console.log('token atualizado');
         console.log(access_token);
         console.log(userObj.devices[1].value);
+        return userObj;
     });
     res.send({status: true, token: access_token});
 }
