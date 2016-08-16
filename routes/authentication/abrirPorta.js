5af4cd1d29ba4db4baf978d9a1179657aab64bbf
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-16 11:26:01 -0300

    metodos para usuario -ADM- logar

--- routes/authentication/abrirPorta.js
+++ routes/authentication/abrirPorta.js
@@ -4,15 +4,7 @@ var Mongo = require("./../../classes/mongo.js");
 var lock = require('./../../actions/lock.js');
 var request = require('request');
 
-token.post('/', function(req, res){
-    /*var tokenGoogle = req.query.token;
-    Mongo.find({devices: { $elemMatch: { value: tokenGoogle } }}, 'user', res, function(res, userObj){
-    	lock('mobile', userObj[0].name);
-    	res.send({status: true});
-    },function(req2, res2){
-        res.send({status: false});
-        console.log('token fail');
-    });*/
+/*token.post('/', function(req, res){
     var tokenGoogle = req.query.token;
     request({
         uri:"https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + tokenGoogle,
@@ -33,8 +25,27 @@ token.post('/', function(req, res){
             }
         }
     )
-});
-    
+});*/
 
+token.post('/', function(req, res){
+    var tokenGoogle = req.query.token;
+    Mongo.find({devices: { $elemMatch: { value: tokenGoogle } }}, 'user', res, function(res, userObj){
+    	request({
+	        uri:"https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + tokenGoogle,
+	        method:'GET'},
+	        function (error, response, body) {
+	            if (!error && response.statusCode == 200) {
+	            	lock('mobile', userObj[0].name);
+			    	res.send({status: true});
+			    }
+            else{
+                console.log('e-mail desativado');
+                res.send({status: false});
+            }
+    },function(req2, res2){
+        res.send({status: false});
+        console.log('token fail');
+    });
+});
 
 module.exports = token;
---
+++ routes/authentication/usuario.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var token = express.Router();
+var Mongo = require("./../../classes/mongo.js");
+var request = require('request');
+
+token.post('/', function(req, res){
+	var s = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    var password = Array.apply(null, Array(N)).map(function() { return s.charAt(Math.floor(Math.random() * s.length)); }).join('');
+    if(req.query.key=="@digitaldesk1" && req.query.user=="admin"){
+    	var name = req.query.user;
+    	Mongo.find({name: name}, 'user', res, function(res, userObj){
+    		var token = userObj[0].devices[1].value;
+	        console.log('usuario encontrado');
+	        conferirToken(req, res, token, password, name);
+	    },function(req2, res2){
+	        console.log('usuario não encontrado');
+	        cadastrarUsuario(req, res, password, name);
+	    });
+    }
+    else {
+        res.send({status: false});
+    }
+});
+
+function cadastrarUsuario(req, res, password, name){
+    var insertObj = {name: name, role: "", status: "A", devices: [{status: "I", name: "touch", value: "", timeRange: ""}, {status: "A", name: "mobile", value: password, timeRange: ""}, {status: "I", name: "nfc", value: "", timeRange: ""}]};    
+    Mongo.insert(insertObj, 'user', function(success){
+        if (success)
+            res.send({status: true, token: password});
+        else res.send({status: false});
+    });
+    console.log('usuario cadastrado');
+
+function conferirToken(req, res, token, password, name){
+    if(password==token){
+        res.send({status: true, token: password});
+        console.log('token correto');
+    }
+    else {
+        atualizarToken(req, res, password, name);
+    }
+}
+
+function atualizarToken(req, res, password, name){
+    var name = {name: name};
+    Mongo.update(name, password, function (success) {
+        if (success)
+            res.send({status: true, token: password});
+        else res.send({status: false});
+    });
+    
+}
