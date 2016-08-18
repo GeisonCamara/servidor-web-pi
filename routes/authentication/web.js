e30784582a2671b807296b2629c5863abc88739d
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-18 16:01:21 -0300

    conseguindo dados da conta

--- routes/authentication/web.js
+++ routes/authentication/web.js
@@ -4,7 +4,9 @@ var Mongo = require("./../../classes/mongo.js");
 var request = require('request');
 
 function onSignIn(response) {
-   location.href="https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com";
+   console.log('Logou');
+   window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com")
+   //location.href="https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com";
 };
 
 module.exports = web;
