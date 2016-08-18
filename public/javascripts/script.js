2a1d9bfaf3117b56dc6e4e48e7177edd5f8d3d2b
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-18 16:07:41 -0300

    conseguindo dados da conta

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -32,6 +32,12 @@ function clickButton(){
 	});
 }
 
+function onSignIn(response) {
+   console.log('Logou');
+   window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com")
+   //location.href="https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com";
+};
+
 $(document).ready(function(){
     hideButton();
     showButton();
--- routes/authentication/web.js
+++ routes/authentication/web.js
@@ -3,10 +3,10 @@ var web = express.Router();
 var Mongo = require("./../../classes/mongo.js");
 var request = require('request');
 
-function onSignIn(response) {
+/*function onSignIn(response) {
    console.log('Logou');
    window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com")
    //location.href="https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com";
-};
+};*/
 
 module.exports = web;
