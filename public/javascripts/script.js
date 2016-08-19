0f09e17dbc5c4756cfa26d8a99c0176834870334
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-19 11:00:19 -0300

    conseguindo dados da conta

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -30,7 +30,7 @@ function onSignIn(response) {
 
         }
     });
-    window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com");
+    //window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com");
 };
 
 function clickButton(){
