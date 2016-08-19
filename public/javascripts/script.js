747c75b7ccc6ff794613e9567679c24b1173350e
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-19 13:38:03 -0300

    conseguindo dados da conta

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -20,7 +20,9 @@ function unlock(){
 
 function onSignIn(response) {
     window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com");
-    console.log(window.location.href);
+    setTimeout(function(){
+        console.log(window.location.href);
+    }, 3000);
     /*setTimeout(function(){
         function queryObj() {
             var result = {}, keyValuePairs = location.search.slice(1).split("&");
