3ce89d9b3df94dd63cd43a9eb093b5f54d0a36ab
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-19 09:45:53 -0300

    conseguindo dados da conta

--- routes/authentication/web.js
+++ routes/authentication/web.js
@@ -7,7 +7,16 @@ function onSignIn(response) {
    console.log('Logou');
    window.location.assign("https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://localhost:3000&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com")
    setTimeout(function(){
-        console.log(window.location.href); //testar delay
+       function queryObj() {
+		    var result = {}, keyValuePairs = location.search.slice(1).split("&");
+		    keyValuePairs.forEach(function(keyValuePair) {
+		        keyValuePair = keyValuePair.split('=');
+		        result[decodeURIComponent(keyValuePair[0])] = decodeURIComponent(keyValuePair[1]) || '';
+		    });
+		    return result;
+		}
+		var myParam = queryObj();
+		console.log(myParam);
     }, 5000);
 };
 
