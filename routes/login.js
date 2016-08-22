72fda2f3c3fedd1959d1691017258b033b12dc89
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-22 14:41:59 -0300

    pegando token da url

--- routes/login.js
+++ routes/login.js
@@ -9,18 +9,17 @@ router.get('/', function(req, res, next) {
 router.get('/Google', function(req, res, next) {
     res.writeHead(301, {'Location': 'https://accounts.google.com/o/oauth2/v2/auth?scope=email%20profile&state=%2Fprofile&redirect_uri=http://porta.digitaldesk.com.br/Login/CompletarGoogle&response_type=token&client_id=489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com'});
 	res.end();
-	console.log('/redirecionar');
 });
 
 router.get('/CompletarGoogle', function(req, res, next) {
-	var url_parts = url.parse(req.url, true);
-	var query = url_parts.query;
-	var url = window.location.pathname;
+	/*var url_parts = url.parse(req.url, true);
+	var query = url_parts.query;*/
+	var url = req.url;
 
 	console.log(url);
 	console.log('access_token - ' + query.access_token);
 	console.log('req - ' + JSON.stringify(query));
-    res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br/?access_token=' + query.access_token});
+    res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br'});
 	res.end();
 });
 
