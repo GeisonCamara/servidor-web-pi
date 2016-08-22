dbef4a9fe20a31142dc9f9c57c1a7befdcf533bf
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-22 13:28:39 -0300

    pegando token da url

--- routes/login.js
+++ routes/login.js
@@ -13,12 +13,11 @@ router.get('/Google', function(req, res, next) {
 });
 
 router.get('/CompletarGoogle', function(req, res, next) {
-	var url_parts = url.parse(req.url, true);
+	var url_parts = url.parse(req.url.replace('#', '?'), true);
 	var query = url_parts.query;
-	var quer = query.replace(/ /gi, "-")
 
-	console.log('access_token - ' + quer.access_token);
-	console.log('req - ' + JSON.stringify(quer));
+	console.log('access_token - ' + query.access_token);
+	console.log('req - ' + JSON.stringify(query));
     res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br'});
 	res.end();
 });
