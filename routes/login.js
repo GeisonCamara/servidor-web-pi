3a1edc2a13a36a4087f9ca6f1aa4ec710491708e
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-23 10:03:34 -0300

    retirada dos - res.send -

--- routes/login.js
+++ routes/login.js
@@ -35,14 +35,85 @@ router.get('/CompletarGoogle', function(req, res, next) {
         if (!error && response.statusCode == 200) {
             var parse = JSON.parse(body);
             var access_token = parse.access_token;
-            //acessarToken(req, res, access_token);
+            acessarToken(req, res, access_token);
             console.log(access_token);
         }
         else{
             console.log('fail na primeira requisição');
-            res.send({status: false}); 
+            //res.send({status: false}); 
         }
     });
 });
+
+function acessarToken(req, res, access_token){
+    request({
+        uri:"https://www.googleapis.com/oauth2/v1/userinfo?access_token=" + access_token,
+        method:'GET'},
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var parse = JSON.parse(body);
+                var name = parse.name;
+                var domain = parse.hd;
+                console.log('nome - ' + name + ' dominio - ' + domain);
+                verificarGrupo(req, res, access_token, name, domain);
+            }
+            else{
+                console.log('fail na segunda requisição');
+                res.send({status: false});
+            }
+        }
+    )
+}
+
+function verificarGrupo(req, res, access_token, name, domain){
+    if (domain == "digitaldesk.com.br") {
+        consultarUsuario(req, res, access_token, name);
+        console.log('e-mail valido');
+    }    
+    else {
+        console.log('email inválido');
+        //res.send({status: false});
+    }
+}
+
+function consultarUsuario(req, res, access_token, name){
+    Mongo.find({name: name}, 'user', res, function(res, userObj){
+        console.log('usuario encontrado');
+        var token = userObj[0].devices[1].value;
+        conferirToken(req, res, token, access_token, name);
+    },function(req2, res2){
+        console.log('usuario não encontrado');
+        cadastrarUsuario(req, res, access_token, name);
+    });
+}
+
+function cadastrarUsuario(req, res, access_token, name){
+    var insertObj = {name: name, role: "", status: "A", devices: [{status: "I", name: "touch", value: "", timeRange: ""}, {status: "A", name: "mobile", value: access_token, timeRange: ""}, {status: "I", name: "nfc", value: "", timeRange: ""}]};    
+    Mongo.insert(insertObj, 'user', function(success){
+        /*if (success)
+            res.send({status: true, token: access_token});
+        else res.send({status: false});
+    });*/
+    console.log('usuario cadastrado');
+}
+
+function conferirToken(req, res, token, access_token, name){
+    if(access_token==token){
+        //res.send({status: true, token: access_token});
+        console.log('token correto');
+    }
+    else {
+        atualizarToken(req, res, access_token, name);
+    }
+}
+
+function atualizarToken(req, res, access_token, name){
+    var name = {name: name};
+    Mongo.update(name, access_token, function (success) {
+        /*if (success)
+            res.send({status: true, token: access_token});
+        else res.send({status: false});
+    });*/
+}
 
 module.exports = router;
