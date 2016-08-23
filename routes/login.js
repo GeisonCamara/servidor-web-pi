7419da4a50945af10d35d9b5448dfc44db61717a
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-23 09:57:23 -0300

    pegando code da url

--- routes/login.js
+++ routes/login.js
@@ -15,10 +15,34 @@ router.get('/Google', function(req, res, next) {
 
 router.get('/CompletarGoogle', function(req, res, next) {
 	var url_parts = url.parse(req.url, true).query;
-
 	console.log('code - ' + JSON.stringify(url_parts.code));
+
     //res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br'});
 	//res.end();
+
+	var codeGoogle = url_parts.code;
+    var headers = {'Content-Type': 'x-www-form-urlencoded'};
+    var body = {grant_type: 'authorization_code',
+                client_id: '489399558653-enu1g23uf2gt1algid9pvfhn8n57tmqt.apps.googleusercontent.com',
+                client_secret: 'nO767Akyi-hCQDkDuQ__i2v0',
+                redirect_uri: 'http://porta.digitaldesk.com.br/Login/CompletarGoogle',
+                code: codeGoogle};
+    request.post({
+        url:"https://www.googleapis.com/oauth2/v4/token",
+        headers:headers,
+        form:body
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var parse = JSON.parse(body);
+            var access_token = parse.access_token;
+            //acessarToken(req, res, access_token);
+            console.log(access_token);
+        }
+        else{
+            console.log('fail na primeira requisição');
+            res.send({status: false}); 
+        }
+    });
 });
 
 module.exports = router;
