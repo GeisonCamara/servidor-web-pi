2a79e97171f0e9e78bfe2543b49288c5483a3a8e
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-25 09:45:59 -0300

    mensagem de e-mail inválido

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -30,11 +30,5 @@ $(document).ready(function(){
 	hideButton();
 	showButton();
 	clickButton();
-	$('#signOut').click(function(){
-		var auth2 = gapi.auth2.getAuthInstance();
-		auth2.signOut().then(function () {
-			console.log('User signed out.');
-		});
-	});
 });
 
