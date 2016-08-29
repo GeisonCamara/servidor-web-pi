5c67390e2d1da4f87e3f55d3041c660808c52ca8
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-08-29 14:05:08 -0300

    Alteração no mustBeConfig

--- routes/authentication/mustBeConfig.js
+++ routes/authentication/mustBeConfig.js
@@ -1,25 +1,58 @@
 var mustBe = require("./../../node_modules/mustbe/mustbe");
 var Mongo = require("./../../classes/mongo.js");
 
-module.exports = function(config){
-    config.routeHelpers(function(rh){
-        rh.getUser(function(req, cb){
+module.exports = function(config) {
+
+    config.userIdentity(function(id) {
+
+        id.isAuthenticated(function(user, cb) {
+            console.log('[mustBe] id.isAuthenticated');
+
+            var isAuthenticated = false;
+            if (user) {
+                isAuthenticated = user.isLoggedIn();
+            }
+            cb(null, isAuthenticated);
+        });
+    });
+
+    config.routeHelpers(function(rh) {
+        
+        rh.getUser(function(req, cb) {
             var tokenCookie = req.cookies["token"];
+
+            console.log('[mustBe] rh.getUser: token: ' + tokenCookie);
+
             Mongo.find({devices: { $elemMatch: { value: tokenCookie } }}, 'user', cb, function(cb, userObj){
-                console.log('success');
-                config.activities(function(activities){
-                    activities.can("index.view", function(identity, params, cb){
-                        identity.isAuthorized(function(err, isAuth){
-                            return cb(null, isAuth);
-                        });
-                    });
-                });
+                cb(null, { token: tokenCookie });
             },function(req2, res2){
-                rh.notAuthenticated(function(req, res, next){
-                    res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br/login/erro'});
-                    res.end();
-                });
+                cb('Usuário inválido');
             });
         });
+
+        rh.notAuthorized(function(req, res, next) {
+            console.log('[mustBe] rh.notAuthorized');
+
+            res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br/login/erro'});
+            res.end();
+        });
+
+        rh.notAuthenticated(function(req, res, next) {
+            console.log('[mustBe] rh.notAuthenticated');
+
+            res.writeHead(301, {'Location': 'http://porta.digitaldesk.com.br/login/erro'});
+            res.end();
+        });       
+
+    });
+
+    config.activities(function(activities) {
+
+        activities.can("index.view", function(identity, params, cb) {
+            console.log('[mustBe] activities.can');
+
+            cb(null, true);
+        });
+
     });
 };
