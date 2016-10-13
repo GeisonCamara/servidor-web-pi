a7f2f3ff529b0f970d03b913ce23d8351c12f9a5
bakebak <brunokl23@hotmail.com>
2016-10-13 15:44:15 -0300

    correção

--- actions/sound.js
+++ actions/sound.js
@@ -2,16 +2,16 @@ module.exports = function(type){
     var Sound = require('node-aplay');
     var config = require('./../classes/config');
     var music = new Sound('/home/pi/node/trava/dev/servidor-web-pi/media/' + config.get('campainha:toque'));
-    var unlock = new Sound('/home/pi/node/trava/dev/servidor-web-pi/media/' + config.get('unlock:toque'));
+    //var unlock = new Sound('/home/pi/node/trava/dev/servidor-web-pi/media/' + config.get('unlock:toque'));
 
     music.play();
 
-    if(type=='campainha'){
+    //if(type=='campainha'){
 	    setTimeout(function(){
 	        music.resume();
 	    }, 1000);
-    }
+    /*}
     else{
     	unlock.resume();
-    }
+    }*/
 }
