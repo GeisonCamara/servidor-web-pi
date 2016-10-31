d4bc87dfe2c705d9d42bbcce73f5e12ab6a0cffb
bakebak <brunokl23@hotmail.com>
2016-10-31 11:06:24 -0200

    add -.js- ao requerir um arquivo

--- actions/beacon.js
+++ actions/beacon.js
@@ -1,3 +1,3 @@
-var eddystoneBeacon = require('./../node_modules/eddystone-beacon/index');
+var eddystoneBeacon = require('./../node_modules/eddystone-beacon/index.js');
 
 eddystoneBeacon.advertiseUid('00010203040506070809', '000000000100');
