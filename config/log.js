781c16b9c5fba793e480a6ac7d0bc87a76edd982
Bruno Klein <bruno.klein@digitaldesk.com.br>
2017-02-08 09:16:14 -0200

    diretorio onde os logs são salvos

--- config/log.js
+++ config/log.js
@@ -4,13 +4,13 @@ require('winston-daily-rotate-file');
 var logger = new (winston.Logger)({
     transports: [
         new winston.transports.DailyRotateFile(
-            {   filename: './../logs/log',
+            {   filename: '~/node/trava/dev/servidor-web-pi/logs/log',
                 datePattern: 'yyyy-MM-dd.',
                 prepend: true,
                 level: 'error',
                 colorize: false,
                 timestamp: true,
-                json: true,
+                json: false,
                 handleExceptions: true
             }
         )
