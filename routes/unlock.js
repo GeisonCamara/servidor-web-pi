c606df85064004f9d3df343b16adba5de4f0fdb8
Bruno Klein <bruno.klein@digitaldesk.com.br>
2017-03-27 14:58:09 -0300

    rota de abrir a porta pelo slack

--- routes/unlock.js
+++ routes/unlock.js
@@ -3,14 +3,30 @@ var router = express.Router();
 var lock = require('./../actions/lock.js');
 var sound = require('./../actions/sound.js');
 var log = require("./../config/log.js");
+var utils = requirre("./../utils")
 
 router.get('/', function(req, res, next) {
-    if(req.query.key=="DD2016TRNEE"){
+    if (req.query.key == "DD2016TRNEE") {
         lock(req.query.device, req.query.user);
         res.sendStatus(200);
-    }
-    else {
+    } else {
         res.sendStatus(404);
+    }
+});
+
+router.get('/slack', function(req, res, next) {
+    var token = req.query.key;
+
+    var tokenSlack = utils.passwordSlack[token];
+    if (!tokenSlack) {
+        res.send({ status: false });
+    } else if (!tokenSlack.date.getTime() + (30 * 1000) > new Date.getTime()) {
+        res.send({ status: false });
+        utils.passwordSlack[token] = undefined;
+    } else {
+        lock("web", "Slack");
+        res.send({ status: true });
+        utils.passwordSlack[token] = undefined;
     }
 });
 
